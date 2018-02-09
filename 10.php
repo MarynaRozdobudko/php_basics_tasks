c40906a0a4fa4f0f47af730676e7f199bdb3dc6a
Shkodenko V. Taras <podlom@users.noreply.github.com>
2018-02-09 12:53:34 +0200

    Update 10.php
    Можно сократить без потерь таким образом

--- 10.php
+++ 10.php
@@ -13,20 +13,10 @@ $day=5;
 
 switch ($day) {
     case 1:
-        echo "Это рабочий день";
-        break;
     case 2:
-        echo "Это рабочий день";
-        break;
     case 3:
-        echo "Это рабочий день";
-        break;
     case 4:
-        echo "Это рабочий день";
-        break;
     case 5:
         echo "Это рабочий день";
         break;
 }
-
-?>
